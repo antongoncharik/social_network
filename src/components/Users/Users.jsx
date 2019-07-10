4bb73814a00b7a4667e7f88848f9b8b3316019e3
Anton Goncharik <ant.goncharik@gmail.com>
2019-07-10 23:23:33 +0300

    20190710

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,68 +1,38 @@
 import React from 'react';
 import s from './Users.module.css';
+import * as axios from 'axios';
+import anonymousAvatar from './../../resource/img/anonymous_avatar.jpg';
 
-const Users = (props) => {
-  if (!props.users.length) {
-    props.setUsers([{
-        id: 1,
-        fullname: 'Roma',
-        age: 24,
-        followed: true,
-        status: 'i am cool',
-        location: {
-          country: 'Belarus',
-          city: 'Minsk'
-        },
-        srcAvatar: 'https://bipbap.ru/wp-content/uploads/2018/03/01-700x1050-640x960.jpg'
-      },
-      {
-        id: 2,
-        fullname: 'Sasha',
-        age: 26,
-        followed: true,
-        status: 'i am cool too',
-        location: {
-          country: 'Belarus',
-          city: 'Minsk'
-        },
-        srcAvatar: 'https://o-viber.ru/wp-content/uploads/2017/08/mujskie_avatarki-5.png'
-      },
-      {
-        id: 3,
-        fullname: 'Nikolay',
-        age: 25,
-        followed: false,
-        status: 'i am cool too',
-        location: {
-          country: 'Russia',
-          city: 'Moskow'
-        },
-        srcAvatar: 'http://www.youloveit.ru/uploads/posts/2016-03/1457531985_youloveit_ru_zveropolis_avatarki02.jpg'
-      }
-    ])
+class Users extends React.Component {
+    componentDidMount() {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        this.props.setUsers(response.data.items);
+      });
   }
-
-  return (
-    props.users.map((user) =>
-      <div key={user.id} className={s.userBlock}>
-        <div>{user.fullname}</div>
-        <div>{user.age}</div>
-        <div className={s.avatarBlock}>
-          <img src={user.srcAvatar}></img>
-        </div>
-        <div>{user.status}</div>
-        <div>{user.location.country}</div>
-        <div>{user.location.city}</div>
-        <div>{user.followed ?
-          <div>
-            <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>
-          </div> :
-          <div>
-            <button onClick={() => {props.follow(user.id)}}>Follow</button>
-           </div>}
-        </div>
-     </div>)
-  )
+  render() {
+      return (
+        this.props.users.map((user) =>
+          <div key={user.id} className={s.userBlock}>
+            <div className={s.avatarBlock}>
+              <img src={user.photos.small === null ? anonymousAvatar : user.photos.small}></img>
+            </div>
+            <div>{user.name}</div>
+            <div>{'user.age'}</div>
+            <div>{'user.status'}</div>
+            <div>{'user.location.country'}</div>
+            <div>{'user.location.city'}</div>
+            <div>{user.followed ?
+              <div>
+                <button onClick={() => {this.props.unfollow(user.id)}}>Unfollow</button>
+              </div> :
+              <div>
+                <button onClick={() => {this.props.follow(user.id)}}>Follow</button>
+               </div>}
+            </div>
+         </div>)
+      )
+  }
 }
 
 export default Users;
