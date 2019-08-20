cb8367e1d563a838ff7ec9deadf678ea33746920
AntonGoncharik <ant.goncharik@gmail.com>
2019-08-20 16:16:00 +0300

    20190820 v1

---
+++ src/components/Profile/ProfileInfo/ProfileIStatusHooks.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import s from './ProfileInfo.module.css';
+
+const ProfileStatusHooks = (props) => {
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+    const disActivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    }
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <div className={s.statusBlock}>
+            {editMode &&
+            <div>
+                <input autoFocus={true}
+                       onBlur={disActivateEditMode}
+                       value={status}
+                       onChange={onStatusChange}/>
+            </div>
+            }
+            {!editMode &&
+            <div>
+                <span onDoubleClick={activateEditMode}>{props.status}</span>
+            </div>
+            }
+        </div>)
+}
+
+export default ProfileStatusHooks;
